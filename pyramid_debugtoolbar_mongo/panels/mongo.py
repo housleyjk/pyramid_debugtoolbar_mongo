0233e49707f9a8cee03b06a7f84ce97c0620fa40
housleyjk <housleyjk@gmail.com>
2014-07-19 16:48:36 -0600

    Update toolbar

--- pyramid_debugtoolbar_mongo/panels/mongo.py
+++ pyramid_debugtoolbar_mongo/panels/mongo.py
@@ -24,35 +24,32 @@ class MongoDebugPanel(DebugPanel):
     """
     name = 'MongoDB'
     has_content = has_mongo
+    template = 'pyramid_debugtoolbar_mongo.panels:templates/mongo.dbtmako'
 
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
         operation_tracker.install_tracker()
         operation_tracker.reset()
 
+    @property
     def nav_title(self):
         return 'MongoDB'
 
+    @property
     def nav_subtitle(self):
         num_operations = len(operation_tracker.queries)
         attrs = ['queries', 'inserts', 'updates', 'removes']
         total_time = sum(sum(o['time'] for o in getattr(operation_tracker, a)) for a in attrs)
         return '{0} operations in {1:.2f}ms'.format(num_operations, total_time)
 
+    @property
     def title(self):
         return 'MongoDB Operations'
 
-    def url(self):
-        return ''
-
-    def content(self):
-        context = {
+    def render_vars(self):
+        return {
             'queries': operation_tracker.queries,
             'inserts': operation_tracker.inserts,
             'updates': operation_tracker.updates,
-            'removes': operation_tracker.removes
+            'moves': operation_tracker.removes
         }
-
-        return self.render(
-            'pyramid_debugtoolbar_mongo.panels:templates/mongo.dbtmako',
-            context, self.request)
